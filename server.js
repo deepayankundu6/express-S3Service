3fde3658a1337dd4b9b5f0a3771f695c083eb242
Deepayan Kundu <deepayankundu6@gmail.com>
2024-01-19 15:55:06 +0530

    fixes and configs

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const app = require("./routes");
 const http = require("http");
 const PORT = 5505;
 const server = http.createServer(app);
+require('dotenv').config()
 
 server.listen(PORT, (err) => {
     if (err) console.log("opps some error occured while setting up the server!!!");
