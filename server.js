d82d421766a3d608fc3b8b5d4225cf6c5dd93ebb
Deepayan Kundu <deepayankundu6@gmail.com>
2024-01-21 19:44:26 +0530

    vrgvvg

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const app = require("./routes");
 const http = require("http");
+require('dotenv').config();
+
 const PORT = 5505;
 const server = http.createServer(app);
-require('dotenv').config()
 
 server.listen(PORT, (err) => {
     if (err) console.log("opps some error occured while setting up the server!!!");
